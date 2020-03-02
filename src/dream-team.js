f8ffc9c5c8c8f53306193668af8917a3c988048d
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-03-02 20:02:49 +0300

    feat: solve 2 tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  if (!Array.isArray(arr)) return false
+  let team = [];
+
+  for (let unit of arr) {
+    if (typeof unit == 'string') {
+      for (let char of unit) {
+        if (char !== ' ') {
+          team.push(char.toUpperCase());
+          break
+        }
+      }
+    }
+  }
+  return team.sort().join('')
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,32 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push(`( ${value} )`)
+    return this
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof position !== 'number') {
+      this.chain = [];
+      throw 'bad value';
+    }
+    if (position > this.chain.length || position <= 0) {
+      this.chain = []
+      throw 'bad value';
+    }
+    this.chain.splice(position - 1, 1);
+    return this
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let rock = this.chain.join('~~');
+    this.chain = []
+    return rock
   }
 };
 
