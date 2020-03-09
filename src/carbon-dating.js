1676aa4c94be5ad769c42ca98cff5f00724bc88c
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-03-09 19:17:00 +0300

    feat: solve carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,18 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (sampleActivity === undefined || typeof sampleActivity !== 'string') return false
+
+  if (sampleActivity.indexOf('.') !== sampleActivity.lastIndexOf('.')) {
+    sampleActivity = sampleActivity.split('');
+    sampleActivity.splice(sampleActivity.lastIndexOf('.'), 1);
+    sampleActivity = sampleActivity.join('');
+  }
+
+  if (isNaN(Number(sampleActivity)) || sampleActivity > 15 || sampleActivity <= 0) return false
+
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let t = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
+  return Math.ceil(t);
 };
